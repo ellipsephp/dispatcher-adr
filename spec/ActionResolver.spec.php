ecc23e93fc3d16520522e9a09bffb3d9bb3a94bf
pmall <pierre.mallinjoud@gmail.com>
2018-03-21 17:24:54 +0100

    add ActionResolver class

---
+++ spec/ActionResolver.spec.php
@@ -0,0 +1,175 @@
+<?php
+
+use function Eloquent\Phony\Kahlan\mock;
+use function Eloquent\Phony\Kahlan\onStatic;
+
+use Psr\Container\ContainerInterface;
+
+use Psr\Http\Server\RequestHandlerInterface;
+
+use Ellipse\Dispatcher;
+use Ellipse\DispatcherFactoryInterface;
+use Ellipse\Dispatcher\ActionResolver;
+use Ellipse\Handlers\ActionRequestHandler;
+use Ellipse\Handlers\ADR\DomainInterface;
+use Ellipse\Handlers\ADR\ResponderInterface;
+
+describe('ActionResolver', function () {
+
+    beforeEach(function () {
+
+        $this->container = mock(ContainerInterface::class)->get();
+        $this->delegate = mock(DispatcherFactoryInterface::class);
+
+        $this->resolver = new ActionResolver($this->container, $this->delegate->get());
+
+    });
+
+    it('should implement DispatcherFactoryInterface', function () {
+
+        expect($this->resolver)->toBeAnInstanceOf(DispatcherFactoryInterface::class);
+
+    });
+
+    describe('->__invoke()', function () {
+
+        beforeEach(function () {
+
+            $this->domain = onStatic(mock(DomainInterface::class))->className();
+            $this->responder = onStatic(mock(ResponderInterface::class))->className();
+
+            $this->dispatcher = mock(Dispatcher::class)->get();
+
+        });
+
+        context('when the given request handler is not an ADR action definition', function () {
+
+            it('should proxy the delegate with the given request handler', function () {
+
+                $test = function ($handler) {
+
+                    $this->delegate->__invoke->with($handler, '~')->returns($this->dispatcher);
+
+                    $test = ($this->resolver)($handler, []);
+
+                    expect($test)->toBe($this->dispatcher);
+
+                };
+
+                $test('Domain'); // Not a domain class name
+                $test([]); // Empty array
+                $test(['Domain']); // Array without a domain class name as first element
+                $test([$this->domain, 'Responder']); // Array without a responder class name as second element
+                $test([$this->domain, $this->responder, 'input']); // Array without an array as third element
+
+            });
+
+        });
+
+        context('when the given request handler is an ADR action definition', function () {
+
+            context('when the ADR action definition is a domain class name', function () {
+
+                it('should use a new ActionRequestHandler using the defined domain, default responder and an empty array of default input', function () {
+
+                    $handler = new ActionRequestHandler($this->container, $this->domain, ResponderInterface::class, []);
+
+                    $this->delegate->__invoke->with($handler, '~')->returns($this->dispatcher);
+
+                    $test = ($this->resolver)($this->domain);
+
+                    expect($test)->toBe($this->dispatcher);
+
+                });
+
+            });
+
+            context('when the ADR action definition is an array', function () {
+
+                context('when no responder class name is defined', function () {
+
+                    it('should use a new ActionRequestHandler using the defined domain, default responder and an empty array of default input', function () {
+
+                        $handler = new ActionRequestHandler($this->container, $this->domain, ResponderInterface::class, []);
+
+                        $this->delegate->__invoke->with($handler, '~')->returns($this->dispatcher);
+
+                        $test = ($this->resolver)([$this->domain]);
+
+                        expect($test)->toBe($this->dispatcher);
+
+                    });
+
+                });
+
+                context('when a responder class name is defined', function () {
+
+                    context('when no default input is defined', function () {
+
+                        it('should use a new ActionRequestHandler using the defined domain and responder and an empty array of default input', function () {
+
+                            $handler = new ActionRequestHandler($this->container, $this->domain, $this->responder, []);
+
+                            $this->delegate->__invoke->with($handler, '~')->returns($this->dispatcher);
+
+                            $test = ($this->resolver)([$this->domain, $this->responder]);
+
+                            expect($test)->toBe($this->dispatcher);
+
+                        });
+
+                    });
+
+                    context('when a default input is defined', function () {
+
+                        it('should use a new ActionRequestHandler using the defined domain, responder and default input', function () {
+
+                            $handler = new ActionRequestHandler($this->container, $this->domain, $this->responder, ['input']);
+
+                            $this->delegate->__invoke->with($handler, '~')->returns($this->dispatcher);
+
+                            $test = ($this->resolver)([$this->domain, $this->responder, ['input']]);
+
+                            expect($test)->toBe($this->dispatcher);
+
+                        });
+
+                    });
+
+                });
+
+            });
+
+        });
+
+        context('when no middleware queue is given', function () {
+
+            it('should proxy the delegate with an empty array', function () {
+
+                $this->delegate->__invoke->with('~', [])->returns($this->dispatcher);
+
+                $test = ($this->resolver)('handler');
+
+                expect($test)->toBe($this->dispatcher);
+
+            });
+
+        });
+
+        context('when an middleware queue is given', function () {
+
+            it('should proxy the delegate with the given middleware queue', function () {
+
+                $this->delegate->__invoke->with('~', ['middleware'])->returns($this->dispatcher);
+
+                $test = ($this->resolver)('handler', ['middleware']);
+
+                expect($test)->toBe($this->dispatcher);
+
+            });
+
+        });
+
+    });
+
+});
---
+++ src/ActionResolver.php
@@ -0,0 +1,117 @@
+<?php declare(strict_types=1);
+
+namespace Ellipse\Dispatcher;
+
+use Psr\Container\ContainerInterface;
+
+use Psr\Http\Server\RequestHandlerInterface;
+
+use Ellipse\Dispatcher;
+use Ellipse\DispatcherFactoryInterface;
+use Ellipse\Handlers\ActionRequestHandler;
+use Ellipse\Handlers\ADR\DomainInterface;
+use Ellipse\Handlers\ADR\ResponderInterface;
+
+class ActionResolver implements DispatcherFactoryInterface
+{
+    /**
+     * The container.
+     *
+     * @var \Psr\Container\ContainerInterface
+     */
+    private $container;
+
+    /**
+     * The delegate.
+     *
+     * @var \Ellipse\DispatcherFactoryInterface
+     */
+    private $delegate;
+
+    /**
+     * Set up a controller resolver with the given container and delegate.
+     *
+     * @param \Psr\Container\ContainerInterface     $container
+     * @param \Ellipse\DispatcherFactoryInterface   $delegate
+     */
+    public function __construct(ContainerInterface $container, DispatcherFactoryInterface $delegate)
+    {
+        $this->container = $container;
+        $this->delegate = $delegate;
+    }
+
+    /**
+     * Proxy the delegate by wrapping ADR action definitions into action request
+     * handlers.
+     *
+     * @param mixed $handler
+     * @param array $middleware
+     * @return \Ellipse\Dispatcher
+     */
+    public function __invoke($handler, array $middleware = []): Dispatcher
+    {
+        $definition = is_string($handler) ? [$handler] : $handler;
+
+        // when the handler is an array.
+        if (is_array($definition)) {
+
+            // set values for domain, responder and default input.
+            $domain = array_shift($definition) ?? null;
+            $responder = array_shift($definition) ?? ResponderInterface::class;
+            $input = array_shift($definition) ?? [];
+
+            // check if the domain, responder and default input are valid.
+            $valid = $this->isValidDomain($domain)
+                && $this->isValidResponder($responder)
+                && $this->isValidDefaultInput($input);
+
+            // set handler as action request handler when the definition is valid.
+            if ($valid) {
+
+                $handler = new ActionRequestHandler($this->container, $domain, $responder, $input);
+
+            }
+
+        }
+
+        return ($this->delegate)($handler, $middleware);
+    }
+
+    /**
+     * Retrun whether the given value is a valid domain id.
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    private function isValidDomain($value): bool
+    {
+        return is_string($value)
+            && is_subclass_of($value, DomainInterface::class, true);
+    }
+
+    /**
+     * Retrun whether the given value is a valid responder id.
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    private function isValidResponder($value): bool
+    {
+        return is_string($value) && (
+            $value == ResponderInterface::class
+            ||
+            is_subclass_of($value, ResponderInterface::class, true)
+        );
+    }
+
+    /**
+     * Retrun whether the given value is a valid default input.
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    private function isValidDefaultInput($value): bool
+    {
+        return is_array($value);
+    }
+}
